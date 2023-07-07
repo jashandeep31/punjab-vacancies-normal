da49a1b05f400902b0d9a5139a0add62b51b1aa0
Jashandeep Singh <jashandeep1659@gmail.com>
2023-07-07 18:24:09 +0530

    0.0.1

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.get("/", (req, res, next) => {
-    res.send("0.0.3");
+    res.send("0.0.69");
 });
 app.use("/api/v1/auth", require("./routes/authRoutes")); // checked
 app.use("/api/v1/job", require("./routes/jobRoutes"));
