42ee29097ec4fdd7582a839c417eb7b20859249e
Jashandeep Singh <jashandeep1659@gmail.com>
2023-07-07 17:42:40 +0530

    0.0.2

--- index.js
+++ index.js
@@ -60,6 +60,8 @@ app.use(
         saveUninitialized: true,
         cookie: {
             maxAge: 1000 * 60 * 60 * 24 * 7, // One Week
+            sameSite: "none",
+            secure: true,
         },
     })
 );
